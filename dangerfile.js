ea428cb2d87fdd5a6edb76ada5bb09bc42f7d99e
afoil <oakira114@gmail.com>
2022-11-11 03:45:08 +0900

    add

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,9 @@
-const hasChangelog = danger.git.modified_files.includes('CHANGELOG.md')
-const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes(
-  '#trivial'
-)
-
-if (!hasChangelog && !isTrivial) {
-  warn('Please add a changelog entry for your changes.')
+var bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(':exclamation: Big PR (' + ++errorCount + ')');
+  markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
 }
+console.log('### branch: ',  danger.github.pr.head.ref)
+console.log('### branch: ',  danger.github.pr.head)
+console.log('### branch: ',  danger.github.pr.base)
+console.log('### branch: ',  danger.github.pr)
